5fab619acc377d293da27cefbee006add1ab92fa
Chris Larsen <chris@theblacknumbers.com>
2024-05-07 20:17:29 -0500

    Move back internal extension

--- Sources/CLAnimatedTabView/CLAnimatedTabView.swift
+++ Sources/CLAnimatedTabView/CLAnimatedTabView.swift
@@ -142,3 +142,31 @@ struct CLTabViewButtonStyle: ButtonStyle {
     }
 }
 #endif
+
+//  referenced from George's answer here: https://stackoverflow.com/questions/64238485/how-to-loop-over-viewbuilder-content-subviews-in-swiftui
+// MARK: TupleView extension
+extension TupleView {
+    var getViews: [AnyView] {
+        makeArray(from: value)
+    }
+    
+    private struct GenericView {
+        let body: Any
+        
+        var anyView: AnyView? {
+            AnyView(_fromValue: body)
+        }
+    }
+    
+    private func makeArray<Tuple>(from tuple: Tuple) -> [AnyView] {
+        func convert(child: Mirror.Child) -> AnyView? {
+            withUnsafeBytes(of: child.value) { ptr -> AnyView? in
+                let binded = ptr.bindMemory(to: GenericView.self)
+                return binded.first?.anyView
+            }
+        }
+        
+        let tupleMirror = Mirror(reflecting: tuple)
+        return tupleMirror.children.compactMap(convert)
+    }
+}
